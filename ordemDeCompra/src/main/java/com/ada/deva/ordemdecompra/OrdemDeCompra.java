cb876a3224a27f03b404fabae640868b90de2447
Ana Thais Senger <anathaissenger@hotmail.com>
2023-02-02 21:57:04 -0300

    Modelo OrdemDeCompra

--- ordemDeCompra/src/main/java/com/ada/deva/ordemdecompra/OrdemDeCompra.java
+++ ordemDeCompra/src/main/java/com/ada/deva/ordemdecompra/OrdemDeCompra.java
@@ -14,8 +14,55 @@ public class OrdemDeCompra {
     private LocalDate data;
     private String tipoMoeda;
     private double valorMoedaEstrangeira;
-    private double valorContacao;
-    private double valorTotal;
+//    private double valorContacao;
+//    private double valorTotal;
     private String numeroAgencia;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getTipoMoeda() {
+        return tipoMoeda;
+    }
+
+    public void setTipoMoeda(String tipoMoeda) {
+        this.tipoMoeda = tipoMoeda;
+    }
+
+    public double getValorMoedaEstrangeira() {
+        return valorMoedaEstrangeira;
+    }
+
+    public void setValorMoedaEstrangeira(double valorMoedaEstrangeira) {
+        this.valorMoedaEstrangeira = valorMoedaEstrangeira;
+    }
+
+    public String getNumeroAgencia() {
+        return numeroAgencia;
+    }
+
+    public void setNumeroAgencia(String numeroAgencia) {
+        this.numeroAgencia = numeroAgencia;
+    }
 }
