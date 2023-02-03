17b4e1c8c8f7ffbca12488c016ed33de50476257
Ana Thais Senger <anathaissenger@hotmail.com>
2023-02-02 21:57:44 -0300

    Modelo OrdemDeCompraService

---
+++ ordemDeCompra/src/main/java/com/ada/deva/ordemdecompra/OrdemDeCompraService.java
@@ -0,0 +1,24 @@
+package com.ada.deva.ordemdecompra;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class OrdemDeCompraService {
+    private final CrudRepository<OrdemDeCompra, String> repository;
+
+    public Optional<OrdemDeCompra> getById(String id){
+        return repository.findById(id);
+    }
+
+    public void add(OrdemDeCompra entity) throws EntidadeDuplicadaException {
+        if(repository.existsById(entity.getId())){
+            throw new EntidadeDuplicadaException();
+        }
+        repository.save(entity);
+    }
+}
